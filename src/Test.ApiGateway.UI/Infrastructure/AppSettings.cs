879cc29bead45b7cf2d6ea2c17a17597e1e4dafb
Ricardo Bicho <bichori@THEAA.IE>
2021-09-28 22:08:20 +0100

    - Initial commit

---
+++ src/Test.ApiGateway.UI/Infrastructure/AppSettings.cs
@@ -0,0 +1,20 @@
+﻿// -------------------------------------------------------------------------------------
+//  <copyright file="AppSettings.cs" company="The AA (Ireland)">
+//    Copyright (c) The AA (Ireland). All rights reserved.
+//  </copyright>
+// -------------------------------------------------------------------------------------
+
+using Customer.Api.Client.Configuration;
+
+namespace Test.ApiGateway.UI.Infrastructure;
+
+/// <summary>
+/// Defines the configuration/settings.
+/// </summary>
+public class AppSettings
+{
+    /// <summary>
+    /// Gets or sets the HTTP client settings.
+    /// </summary>
+    public Dictionary<string, HttpClientSettings> HttpClientSettingsDictionary { get; set; } = new Dictionary<string, HttpClientSettings>();
+}
